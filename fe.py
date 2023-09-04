a62c0a07795f4d07034a858779789e393c3047f3
Liam Humphreys <liamjameshumphreys@gmail.com>
2023-09-04 20:02:22 +0100

    working e2e pipeline

--- fe.py
+++
@@ -1,7 +0,0 @@
-from ufc_scraper.processors import FeatureEngineeringProcessor
-
-
-fe = FeatureEngineeringProcessor(
-    csv_path="ufc_scraper/data/clean_ufc_data.csv", allow_creation=True
-)
-fe.run_feature_engineering()
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from ufc_scraper.pipelines import ScrapingPipeline
-
+from ufc_scraper.pipelines import ScrapingPipeline, DataCleaningPipeline
+from ufc_scraper.feature_engineering import FeatureEngineeringProcessor
+from ufc_scraper.config import PathSettings
 
 def main():
     scraping_pipeline = ScrapingPipeline()
     scraping_pipeline.run_pipeline()
-
+    data_cleaning = DataCleaningPipeline()
+    data_cleaning.run_pipeline()
+    feature_engineering = FeatureEngineeringProcessor(csv_path = PathSettings.CLEAN_DATA_CSV, allow_creation = False)
+    feature_engineering.run_feature_engineering()
 
 if __name__ == "__main__":
     main()
