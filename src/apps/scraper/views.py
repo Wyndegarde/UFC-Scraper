aeb3fb12e53023a88027716ba8797bed475a3a35
Liam Humphreys <liamjameshumphreys@gmail.com>
2025-02-10 13:11:33 +0000

    updated view

--- src/apps/scraper/views.py
+++ src/apps/scraper/views.py
@@ -3,10 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from src.config.config import PathSettings
+from src.lib.engines import ScrapingEngine
 from src.lib.pipelines import ScrapingPipeline
+from src.lib.processing import JSONCache, CSVProcessingHandler
 
 
 async def scrape_past_events(request):
-    scraping_pipeline = ScrapingPipeline()
-    await scraping_pipeline.run()
+    scraping_pipeline = ScrapingPipeline(
+        ScrapingEngine(),
+        JSONCache(PathSettings.EVENT_CACHE_JSON),
+    )
+    raw_data_processor = CSVProcessingHandler(
+        PathSettings.RAW_DATA_CSV, allow_creation=True
+    )
+    await scraping_pipeline.run(raw_data_processor)
     return HttpResponse("Scraping past events")
